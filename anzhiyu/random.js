81529e70c2f824c8758026a385bff6fe15f48abb
chuxos <464739396@qq.com>
2024-09-14 10:16:37 +0800

    Site updated: 2024-09-14 10:16:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/12/hello-world/"];function toRandomPost(){
+var posts=["2024/09/12/hello-world/","2024/09/14/测试第一稿/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
