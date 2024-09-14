d7d53adf86e773dc248853e167be61ae0d35fdf3
chuxos <464739396@qq.com>
2024-09-14 11:20:23 +0800

    Site updated: 2024-09-14 11:20:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/12/hello-world/","2024/09/14/测试第一稿/"];function toRandomPost(){
+var posts=["2024/09/12/hello-world/","2024/09/14/测试第一稿/","2024/09/14/番茄工作法技巧一：红线原则 提升专注力的基础/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
